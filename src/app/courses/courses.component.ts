a92ae466c86e7ee9a715d52fd5e95a13355361a4
pankaj485 <pankajsamar2@gmail.com>
2023-12-07 23:13:54 +0530

    fix: added courses with ngFor, added event handler to select course, displayed raw JSON of selcted course

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
   courses = [
@@ -12,13 +12,35 @@ export class CoursesComponent implements OnInit {
       title: 'Angular 13 Fundamentals',
       description: 'Learn the fundamentals of Angular 13',
       percentComplete: 26,
-      favorite: true
-    }
+      favorite: true,
+    },
+    {
+      id: 2,
+      title: 'Node Js Fundamentals',
+      description: 'Learn the fundamentals of Node Js',
+      percentComplete: 26,
+      favorite: true,
+    },
+    {
+      id: 3,
+      title: 'JS Fundamentals',
+      description: 'Learn the fundamentals of JavaScript',
+      percentComplete: 16,
+      favorite: true,
+    },
   ];
 
-  constructor() { }
+  selectedCourse = null;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  selectCourse(course) {
+    this.selectedCourse = course;
   }
 
+  removeCourse(courseId) {
+    console.log('COURSE TO REMOVE: ', courseId);
+  }
+
+  ngOnInit(): void {}
 }
